18497509a1e11f39a214850932b739cdefc6f81e
campusboy87 <dmh87@mail.ru>
2017-10-17 04:10:02 +0300

    Обновлены медиа

--- assets/cf7sp-admin.js
+++ assets/cf7sp-admin.js
@@ -2,8 +2,7 @@ jQuery(document).ready(function ($) {
 
     console.log(11);
 
-    var $tabs = $("#tabs");
+    var $tabs = $("#post-type-tabs");
     $tabs.tabs().addClass("ui-tabs-vertical ui-helper-clearfix");
     $("li", $tabs).removeClass("ui-corner-top").addClass("ui-corner-left");
-
 });
