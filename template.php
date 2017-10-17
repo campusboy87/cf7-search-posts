f17b3d14ef3d57d8079a9485dfbef67148db21c6
campusboy87 <dmh87@mail.ru>
2017-10-17 04:09:24 +0300

    Наброски шаблона вкладки

--- template.php
+++ template.php
@@ -2,36 +2,38 @@
 /**
  * Шаблон контента вкладки
  *
- * @var WPCF7SP $this
- */
-?>
-    <h2>Поиск формы в постах</h2>
-
-    <div id="tabs">
-        <ul>
-            <li><a href="#tabs-1">Nunc tincidunt</a></li>
-            <li><a href="#tabs-2">Proin dolor</a></li>
-            <li><a href="#tabs-3">Aenean lacinia</a></li>
-        </ul>
-        <div id="tabs-1">
-            <h2>Content heading 1</h2>
-            <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus.</p>
-        </div>
-        <div id="tabs-2">
-            <h2>Content heading 2</h2>
-            <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id
-                nunc.</p>
-        </div>
-        <div id="tabs-3">
-            <h2>Content heading 3</h2>
-            <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti.</p>
-        </div>
-    </div>
-
-<?php
-/**
+ * @var WPCF7SP      $this
  * @var WP_Post_Type $post_type
  */
-foreach ( $this->allow_post_types() as $post_type ) {
-	printf( '<p><a class="button-primary" href="#%s">%s</a></p>', $post_type->name, $post_type->label );
-}
+
+$post_types = $this->allow_post_types();
+
+if ( $post_types ): ?>
+
+    <h2>Поиск формы в постах</h2>
+
+    <div id="post-type-tabs">
+
+        <!-- Вкладки с типами постов -->
+        <ul>
+			<?php foreach ( $post_types as $post_type ): ?>
+                <li>
+                    <a data-post-type="<?php echo esc_attr( $post_type->name ); ?>"
+                       href="#tabs-<?php echo esc_attr( $post_type->name ); ?>">
+						<?php echo esc_attr( $post_type->label ); ?>
+                    </a>
+                </li>
+			<?php endforeach; ?>
+        </ul>
+
+        <!-- Контент вкладок -->
+		<?php foreach ( $post_types as $post_type ): ?>
+            <div id="tabs-<?php echo esc_attr( $post_type->name ); ?>">
+                <h2><?php echo esc_html( $post_type->label ); ?></h2>
+                <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus.</p>
+            </div>
+		<?php endforeach; ?>
+
+    </div>
+
+<?php endif;
